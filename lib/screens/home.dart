2ac86e5413979ffd7a97fbcc34e64d557d2fa76a
scott thoo <scott.thoo@sofax.com>
2020-11-28 20:34:11 +0800

    v0.0.2 [ADD] Connectivity dependencies. [ADD] Check wifi connectivity. [ADD] Check specific wifi name. [ADD] UI In or out of the zone. [MOD] Init UI at Home Screen, confirm get permission and service first before allow to next page.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geofence/screens/map.dart';
+import 'package:geofence/utilities/constants.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart' hide LocationAccuracy;
+import 'package:connectivity/connectivity.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -14,6 +17,7 @@ class _HomeState extends State<Home> {
   PermissionStatus _permissionGranted;
   LocationData _locationData;
   Position _position;
+  bool isInitPosition = false;
 
   @override
   void initState() {
@@ -39,25 +43,61 @@ class _HomeState extends State<Home> {
     }
     _locationData = await location.getLocation();
     _position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
     print('_position : ${_position}');
+    setState(() {
+      isInitPosition = true;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-            child: RaisedButton(
-          onPressed: () {
-            Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => Map(
-                          location: _position,
-                        )));
-          },
-          child: Text('Go'),
-        )),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF00AEFF),
+              Color(0xFF0076FF),
+            ],
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                  child: (isInitPosition == true)
+                      ? RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: BorderSide(color: kPrimaryColor),
+                          ),
+                          color: kSecondaryColor,
+                          onPressed: () {
+                            print('_position 2: ${_position}');
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Map(
+                                          location: _position,
+                                        )));
+                          },
+                          child: Text(
+                            "Let's go!",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      : CircularProgressIndicator(
+                          valueColor: new AlwaysStoppedAnimation<Color>(Colors.white),
+                        )),
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -1,6 +1,8 @@
 import 'dart:collection';
-
+import 'package:connectivity/connectivity.dart';
+import 'package:cross_connectivity/cross_connectivity.dart' hide Connectivity;
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geofence/utilities/constants.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -17,17 +19,30 @@ class _MapState extends State<Map> {
   Set<Circle> _circles = HashSet<Circle>();
   double radius = 50.0;
   int _circleIdCounter = 1;
+  final Connectivity _connectivity = Connectivity();
+  String _connectionStatus = 'Unknown';
+  String specificWifiName = 'Specific Wifi Name';
+  Future<String> _futureGetWifiName;
+  bool isInTheZone = false;
+  bool isConnectedToSpecificWifi;
 
   @override
   void initState() {
     super.initState();
     _locationData = widget.location;
     print('_locationData : $_locationData');
+    initConnectivity();
+    _futureGetWifiName = _connectivity.getWifiName();
   }
 
   void _setCircles(LatLng point) {
     double distanceInMeters = Geolocator.distanceBetween(_locationData.latitude, _locationData.longitude, point.latitude, point.longitude);
     print('distanceInMeters : $distanceInMeters');
+
+    if (distanceInMeters < radius)
+      setState(() => isInTheZone = true);
+    else
+      setState(() => isInTheZone = false);
 
     final String circleIdVal = 'circle_id_$_circleIdCounter';
     _circleIdCounter++;
@@ -45,9 +60,23 @@ class _MapState extends State<Map> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Geo-fence'),
+        title: Text(
+          'Geofence',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: kPrimaryColor,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              showChangeSpecificWifi();
+            },
+            child: Icon(Icons.settings),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
@@ -73,6 +102,81 @@ class _MapState extends State<Map> {
                 _setCircles(point);
               });
             },
+          ),
+          SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              color: Colors.white,
+              child: Center(
+                child: ConnectivityBuilder(builder: (context, isConnected, status) {
+                  return FutureBuilder<String>(
+                      future: _futureGetWifiName,
+                      builder: (context, AsyncSnapshot<String> wifiName) {
+                        (wifiName.data == specificWifiName) ? isConnectedToSpecificWifi = true : isConnectedToSpecificWifi = false;
+                        if (status != ConnectivityStatus.wifi) isConnectedToSpecificWifi = false;
+
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(width: 15),
+                                Icon(Icons.wifi_lock),
+                                SizedBox(width: 8),
+                                Text('$specificWifiName'),
+                                SizedBox(width: 8),
+                                Icon(Icons.link),
+                                SizedBox(width: 8),
+                                Text(status == ConnectivityStatus.wifi ? '${wifiName.data}' : 'Disconnected'),
+                              ],
+                            ),
+                            SizedBox(width: 15),
+                            Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width,
+                              color: isInTheZone ? Colors.lightGreenAccent : Colors.yellowAccent,
+                              child: Center(
+                                child: Text(
+                                  isInTheZone ? 'IN THE ZONE' : 'NOT IN THE ZONE',
+                                  style: TextStyle(color: Colors.blueGrey),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width,
+                              color: isConnectedToSpecificWifi ? Colors.lightGreenAccent : Colors.redAccent,
+                              child: Center(
+                                child: Text(
+                                  isConnectedToSpecificWifi ? 'INSIDE' : 'OUTSIDE',
+                                  style: TextStyle(color: Colors.blueGrey),
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      });
+
+                  // return Row(
+                  //   mainAxisSize: MainAxisSize.min,
+                  //   children: <Widget>[
+                  //     Icon(
+                  //       isConnected == true ? Icons.signal_wifi_4_bar : Icons.signal_wifi_off,
+                  //       color: isConnected == true ? Colors.green : Colors.red,
+                  //     ),
+                  //     const SizedBox(width: 8),
+                  //     Text(
+                  //       '$status',
+                  //       style: TextStyle(
+                  //         color: status != ConnectivityStatus.none ? Colors.green : Colors.red,
+                  //       ),
+                  //     ),
+                  //   ],
+                  // );
+                }),
+              ),
+            ),
           ),
         ],
       ),
@@ -123,5 +227,85 @@ class _MapState extends State<Map> {
                 )),
           ],
         ));
+  }
+
+  void showChangeSpecificWifi() {
+    showDialog(
+        context: context,
+        child: AlertDialog(
+          backgroundColor: Colors.grey[900],
+          title: Text(
+            'Change Specific Wifi Name',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          content: Padding(
+              padding: EdgeInsets.all(8),
+              child: Material(
+                color: Colors.black,
+                child: TextField(
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.wifi),
+                    hintText: 'myWifi',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (input) {
+                    setState(() {
+                      specificWifiName = input;
+                    });
+                  },
+                ),
+              )),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () {
+                  setState(() {
+                    _circles.clear();
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Ok',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+          ],
+        ));
+  }
+
+  Future<void> initConnectivity() async {
+    ConnectivityResult result;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      result = await _connectivity.checkConnectivity();
+    } on PlatformException catch (e) {
+      print(e.toString());
+    }
+    if (!mounted) {
+      return Future.value(null);
+    }
+
+    return _updateConnectionStatus(result);
+  }
+
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    switch (result) {
+      case ConnectivityResult.wifi:
+        var wifiName = await _connectivity.getWifiName();
+        print('android wifi');
+        print(wifiName);
+        setState(() {
+          _connectionStatus = result.toString();
+        });
+        break;
+      case ConnectivityResult.mobile:
+      case ConnectivityResult.none:
+        print('Disconnected Wifi');
+        break;
+      default:
+        setState(() => _connectionStatus = 'Failed to get connectivity.');
+        break;
+    }
   }
 }
